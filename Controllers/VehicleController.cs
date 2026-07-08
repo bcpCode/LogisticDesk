e68f502122a206d135d2870100e5a0d96cec7821
bcpCode <206368248+bcpCode@users.noreply.github.com>
2026-07-08 14:47:55 +0300

    Added Controller, Implement async and await, Implement SwaggerUI for the development

---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,49 @@
+﻿using LogisticDesk.Data;
+using LogisticDesk.DTOs;
+using LogisticDesk.Enums;
+using LogisticDesk.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace LogisticDesk.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        // Veritabanımızı (mutfağı) temsil eden değişken
+        private readonly LogiDbContext _context;
+
+        // Kurucu metot (Constructor): Garson işe başladığı an mutfağın anahtarı ona verilir
+        public VehicleController(LogiDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetVehicles()
+        {
+            var vehicles = await _context.Vehicles.ToListAsync();
+            return Ok(vehicles);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddVehicle([FromBody] VehicleAddingDto vehicleDto)
+        {
+            if (vehicleDto == null)
+            {
+                return BadRequest("Vehicle data is required.");
+            }
+            Vehicle newVehicle = new Vehicle
+            {
+                PlateNumber = vehicleDto.PlateNumber,
+                MaxCapacity = Math.Round(vehicleDto.MaxCapacity,2),
+                Status = VehicleStatus.Available,
+                CreatedAt = DateTime.UtcNow,
+                IsActive = false
+            };
+            _context.Vehicles.Add(newVehicle);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ DTOs/LogisticsDtos.cs
@@ -0,0 +1,19 @@
+﻿namespace LogisticDesk.DTOs
+{
+    public class LogisticsDtos
+    {
+        public string TrackingNumber { get; set; }
+        public decimal Weight { get; set; }
+        public string ReceiverAddress { get; set; }
+        public int? VehicleId { get; set; }
+
+    }
+}
+namespace LogisticDesk.DTOs
+{
+    public class VehicleAddingDto
+    {
+        public string PlateNumber { get; set; }
+        public decimal MaxCapacity { get; set; }
+    }
+}
